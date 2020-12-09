a64156420aaaaec2c91bd39a862a596cc2715fe5
guyi <lamp1098335737@163.com>
2020-12-09 14:19:16 +0800

    添加im

--- initialize/router.go
+++ initialize/router.go
@@ -3,16 +3,21 @@ package initialize
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"study-gorm/router"
 )
 
 func Routers() *gin.Engine {
 	var Router = gin.Default()
-	//PublicGroup := Router.Group("")
-	//{
-	//	router.InitBaseRouter(PublicGroup) // 注册基础功能路由 不做鉴权
-	//}
+	PublicGroup := Router.Group("")
+	{
+		router.InitBaseRouter(PublicGroup) // 注册基础功能路由 不做鉴权
+	}
+	PrivateGroup := Router.Group("")
+	{
+		router.InitUserRouter(PrivateGroup) // 注册用户路由
+	}
 	Router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "first example.")
+		c.String(http.StatusOK, "first example. dd")
 	})
 	return Router
 }
--- router/sys_base.go
+++ router/sys_base.go
@@ -8,7 +8,7 @@ import (
 func InitBaseRouter(Router *gin.RouterGroup) (R gin.IRoutes) {
 	BaseRouter := Router.Group("base")
 	{
-		BaseRouter.POST("login", v1.Login)
+		BaseRouter.GET("login", v1.Login)
 	}
 	return BaseRouter
 }
